cc319894b4e639c6644f20378050d3520b620ff6
Louistam888 <louistam888@gmail.com>
2025-10-26 15:02:47 -0400

    352

--- internal/trade/trade.go
+++ internal/trade/trade.go
@@ -1,1 +1,16 @@
 package trade
+
+import (
+	"github.com/louistam888/stockAnalysis/internal/news"
+	"github.com/louistam888/stockAnalysis/internal/pos"
+)
+
+type Selection struct {
+	Ticker string
+	pos.Position
+	Articles []news.Article
+}
+
+type Deliverer interface {
+	Deliver(selections []Selection) error
+}
--- pkg/salpha/seeking_alpha.go
+++ pkg/salpha/seeking_alpha.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/louistam888/stockAnalysis/internal/news"
 )
@@ -20,36 +21,51 @@ type client struct {
 	apiKey  string
 }
 
+// Fetch retrieves articles for a given ticker
 func (c *client) Fetch(ticker string) ([]news.Article, error) {
-	url, err := c.buildURL(ticker)
+	fullURL, err := c.buildURL(ticker)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Add(apiKeyHeader, c.apiKey)
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	httpClient := &http.Client{}
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("unsuccessful status code: %d", resp.StatusCode)
+	}
+
+	return c.parse(resp)
 }
 
+// parse converts the HTTP response into news.Article slices
 func (c *client) parse(resp *http.Response) ([]news.Article, error) {
 	res := &SeekingAlphaResponse{}
-	err := json.NewDecoder(resp.Body).Decode(res)
-	if err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(res); err != nil {
 		return nil, err
 	}
 
 	var articles []news.Article
 	for _, item := range res.Data {
-		art := news.Artile{
-			PublishOn: item.Attributes.PublishOn,
+		// Convert PublishOn string to time.Time
+		publishTime, err := time.Parse(time.RFC3339, item.Attributes.PublishOn)
+		if err != nil {
+			// skip invalid dates
+			continue
+		}
+
+		art := news.Article{
+			PublishOn: publishTime,
 			Headline:  item.Attributes.Title,
 		}
 		articles = append(articles, art)
@@ -57,6 +73,7 @@ func (c *client) parse(resp *http.Response) ([]news.Article, error) {
 	return articles, nil
 }
 
+// buildURL constructs the full API URL for a ticker
 func (c *client) buildURL(ticker string) (string, error) {
 	parsedURL, err := url.Parse(c.baseURL)
 	if err != nil {
@@ -64,14 +81,30 @@ func (c *client) buildURL(ticker string) (string, error) {
 	}
 
 	parsedURL.Path += urlPath
+
 	params := url.Values{}
 	params.Add("size", fmt.Sprint(pageSize))
 	params.Add("id", ticker)
-	parsedURL, RawQuery = params.Encode()
+	parsedURL.RawQuery = params.Encode()
 
 	return parsedURL.String(), nil
 }
 
+// NewClient returns a news.Fetcher implementation
 func NewClient(baseURL, apiKey string) news.Fetcher {
 	return &client{baseURL: baseURL, apiKey: apiKey}
+}
+
+// ---- Helper types for JSON parsing ----
+type attributes struct {
+	PublishOn string `json:"publishOn"`
+	Title     string `json:"title"`
+}
+
+type seekingAlphaNews struct {
+	Attributes attributes `json:"attributes"`
+}
+
+type SeekingAlphaResponse struct {
+	Data []seekingAlphaNews `json:"data"`
 }
